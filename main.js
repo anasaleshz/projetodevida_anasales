38574e356259b05a5b88c67313a13bac167b15d5
anasaleshz <143184784+anasaleshz@users.noreply.github.com>
2024-03-18 07:51:42 -0300

    ..

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const botoes = document.querySelectorAll("botao");
+const texto = document.querySelectorAll(".aba-conteudo");
+
 for (let i = 0; <botoes.length; i++) {
     botoes[i].onclik = function () {
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
